13bfa12817882b0c71ecfd234d1106c657c68145
Antonio Zamora <zamora88875@gmail.com>
2022-02-12 19:14:39 -0500

    Much progress.

--- components/ItemDisplay.js
+++ components/ItemDisplay.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import styles from '../styles/ItemDisplay.module.css'
 
 export default function ItemDisplay({ products }) {
 	return (
 		<>
+			{/* Container for entire product display */}
+
 			{products.map((product) => (
-				<h3>{product.title}</h3>
+				<div className={styles.productCard}>
+					<figure>
+						<img src={product.image}></img>
+						<figcaption>A thing!</figcaption>
+					</figure>
+				</div>
 			))}
 		</>
 	)
