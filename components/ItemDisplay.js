ef06f7ca8b4254b7d2eaba5d96b368f07b3b14dc
Antonio Zamora <zamora88875@gmail.com>
2022-02-16 00:08:38 -0500

    Progress with multiple elements.

--- components/ItemDisplay.js
+++ components/ItemDisplay.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect, useLayoutEffect } from 'react'
 import styles from '../styles/ItemDisplay.module.css'
 
-export default function ItemDisplay({ products }) {
+export default function ItemDisplay({ products, display, sort }) {
+	const [productDisplay, setProductDisplay] = useState(products)
+
+	const sortLowToHigh = () => {
+		// sort
+		let tempProducts = products
+		// console.log(tempProducts)
+		tempProducts.sort((a, b) => {
+			return a.price - b.price
+		})
+		// console.log(tempProducts)
+		setProductDisplay(tempProducts)
+	}
+
+	useLayoutEffect(() => {
+		if (sort === 'sortLowestPrice') {
+			// sort function
+			console.log('hi')
+			sortLowToHigh()
+		} else if (sort === 'sortHighestPrice') {
+			//sort function
+		}
+	}, [sort])
+
 	return (
 		<>
 			{/* Container for entire product display */}
 
-			{products.map((product) => (
+			{productDisplay.map((product) => (
 				<div className={styles.productCard}>
 					<figure>
 						<div className={styles.ImgContainer}>
