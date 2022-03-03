82dc8d86a4d97fcb5587a6b8b162fe9c2ad865b1
Antonio Zamora <zamora88875@gmail.com>
2022-03-03 16:04:44 -0500

    Added functioning star system. Needs unique keys.

--- components/ItemDisplay.js
+++ components/ItemDisplay.js
@@ -45,7 +45,7 @@ export default function ItemDisplay({ products, range, sort, custom }) {
 	const sortTopReview = () => {
 		let tempProducts = [...product]
 		tempProducts.sort((a, b) => {
-			return a.rating.rate - b.rating.rate
+			return b.rating.rate - a.rating.rate
 		})
 		setProductDisplay(tempProducts)
 	}
@@ -93,6 +93,93 @@ export default function ItemDisplay({ products, range, sort, custom }) {
 		return <>Sorry no results.</>
 	}
 
+	const fullStar = (
+		<svg
+			version="1.1"
+			xmlns="http://www.w3.org/2000/svg"
+			x="0px"
+			y="0px"
+			viewBox="0 0 441.5 414.5"
+			xmlSpace="preserve"
+			width="14.4px"
+		>
+			<path
+				d="M220.8,0l50,154.8v0c0.8,2.3,2.9,3.9,5.3,3.9l162.6-0.3l-131.7,95.3v0c-2,1.4-2.8,4-2,6.3l50.6,154.6
+				l-131.4-95.9c-2-1.4-4.6-1.4-6.6,0L86.1,414.5l50.6-154.6v0c0.8-2.3-0.1-4.9-2-6.3L2.9,158.3l162.6,0.3v0c2.4,0,4.6-1.6,5.3-3.9
+				L220.8,0z"
+			/>
+		</svg>
+	)
+	const halfStar = (
+		<svg
+			version="1.1"
+			id="Layer_1"
+			xmlns="http://www.w3.org/2000/svg"
+			x="0px"
+			y="0px"
+			viewBox="0 0 435.7 414.4"
+			xmlSpace="preserve"
+			width="14.4px"
+		>
+			<path
+				d="M217.9,0L217.9,0l-50,154.7c-0.8,2.3-2.9,3.9-5.3,3.9v0L0,158.2l131.7,95.3c2,1.4,2.8,4,2,6.3v0L83.2,414.4
+l131.3-95.9c1-0.7,2.2-1.1,3.3-1.1V0z"
+			/>
+			<path
+				fill="#BDBDBD"
+				d="M304,253.5L304,253.5l131.7-95.3l-162.6,0.3c-2.4,0-4.6-1.6-5.3-3.9v0L217.9,0v317.5c1.1,0,2.3,0.4,3.3,1.1
+l131.4,95.9L302,259.8C301.2,257.5,302,255,304,253.5z"
+			/>
+		</svg>
+	)
+	const emptyStar = (
+		<svg
+			version="1.1"
+			id="Layer_1"
+			xmlns="http://www.w3.org/2000/svg"
+			x="0px"
+			y="0px"
+			viewBox="0 0 435.7 414.4"
+			xmlSpace="preserve"
+			width="14.4px"
+		>
+			<path
+				fill="#BDBDBD"
+				d="M217.9,0L217.9,0l-50,154.7c-0.8,2.3-2.9,3.9-5.3,3.9v0L0,158.2l131.7,95.3c2,1.4,2.8,4,2,6.3v0L83.2,414.4
+l131.3-95.9c1-0.7,2.2-1.1,3.3-1.1V0z"
+			/>
+			<path
+				fill="#BDBDBD"
+				d="M304,253.5L304,253.5l131.7-95.3l-162.6,0.3c-2.4,0-4.6-1.6-5.3-3.9v0L217.9,0v317.5c1.1,0,2.3,0.4,3.3,1.1
+l131.4,95.9L302,259.8C301.2,257.5,302,255,304,253.5z"
+			/>
+		</svg>
+	)
+
+	function RatingDisplay({ rating }) {
+		let roundedRate = Math.round(rating / 0.5) * 0.5
+
+		let stars = []
+
+		if (stars.length === 0) {
+			for (let i = 0; i < Math.floor(roundedRate); i++) {
+				stars.push(fullStar)
+			}
+			if (roundedRate !== Math.trunc(roundedRate)) {
+				stars.push(halfStar)
+			}
+			if (stars.length < 5) {
+				let tempArr = [...stars]
+				tempArr.length = 5
+				tempArr.fill(emptyStar, stars.length)
+				stars = tempArr
+			}
+			console.log('A Rating has been calculated!')
+		}
+
+		return stars
+	}
+
 	return (
 		<>
 			{/* Container for entire product display */}
@@ -111,13 +198,8 @@ export default function ItemDisplay({ products, range, sort, custom }) {
 								</div>
 								<div className={styles.productInfo}>
 									<div className={styles.title}>{product.title}</div>
-									{/* <div
-									class="stars"
-									style="--rating: 2.3;"
-									style={{ ['--rating']: '2.3' }}
-									aria-label="Rating of this product is 2.3 out of 5."
-									></div> */}
-									<h2>${product.price}</h2>
+									<RatingDisplay rating={product.rating.rate} />
+									{/*  */}({product.rating.count})<h2>${product.price}</h2>
 								</div>
 							</div>
 						</a>
